0bfed3b13d077eabdd44aeef7d9eb52ab27b5aa8
JConigrave <james.conigrave@gmail.com>
2018-09-05 13:22:37 +1000

    change threshold to percent

--- R/find_similar.R
+++ R/find_similar.R
@@ -3,15 +3,19 @@
 #' Takes in two vectors of strings. This function will return strings in the second set, which are similar to those in the first.
 #' @param x a vector of strings.
 #' @param y a vector of strings. Strings will be returned from y which are similar to those in x
-#' @param threshold a numeric. Strings in y will be returned if they are less than this distance away from x
+#' @param percent a numeric. Strings in y will be returned if they are at least this percent similar. Defaults to 10%.
 #' @return a vector containing strings from y, which are similar to those in x.
 #' @export find_similar
 #' @importFrom stringdist stringdist
 #' @importFrom magrittr %>%
 
-find_similar = function(x, y, threshold = 4) {
+find_similar = function(x, y, percent = 10){
   results = lapply(x, function(i) {
-    y[stringdist(i, y) < threshold]
+    threshold = nchar(i)*(percent/100)
+    if(threshold<1){
+      threshold =1
+    }
+    y[stringdist(i, y) <= threshold]
   })  %>%  unlist
   results[order(results)] %>%
     unique %>%
